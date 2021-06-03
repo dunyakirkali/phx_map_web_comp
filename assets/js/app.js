78ccd03a2a3a000bfcf0009998482cacdb0ac8ea
Dunya Kirkali <dunyakirkali@gmail.com>
2021-06-03 21:30:34 +0200

    Finalize typescript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,4 +12,8 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
+// import 'hello-world' from "./hello-world"
 import "phoenix_html"
+import greet from "./hello";
+
+document.querySelector(".alert-danger").innerHTML = greet("Phoenix");
